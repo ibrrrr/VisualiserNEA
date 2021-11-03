7fb1429ec3f8a546616b04cd6dd94e05a4f8969d
ibrrrr <88161292+ibrrrr@users.noreply.github.com>
2021-11-03 19:28:10 +0000

    Update CNN_Model.py

--- CNN_Model.py
+++ CNN_Model.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import warnings
+from keras.utils import np_utils
 
 warnings.filterwarnings("ignore")
 plt.rcParams['figure.figsize'] = [20, 20]
@@ -18,18 +19,17 @@ def show_images(images, labels, shape=(3,3)):
             ax.set_title(labels[i])
             i += 1
 from tensorflow.keras.datasets import mnist
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-(image_train_mnist, label_train_mnist), (image_test_mnist, label_test_mnist) = mnist.load_data()
-image_mnist = np.concatenate((image_train_mnist, image_test_mnist))
-label_mnist = np.concatenate((label_train_mnist, label_test_mnist))
-image_mnist = image_mnist.reshape(-1,28,28,1)
-image_mnist = image_mnist.astype(np.float32) / 255
-label_mnist = tf.keras.utils.to_categorical(label_mnist,num_classes=10)
-images = np.concatenate((train_image, image_mnist))
-labels = np.concatenate((train_label, label_mnist))
+X_train = X_train.reshape(X_train.shape[0], 28, 28, 1) 
+X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
 
-# final dataset shape
-print("training image dataset shape:", images.shape)
-print("training label dataset shape:", labels.shape)
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
 
-show_images(images[:25], labels[:25], shape=(5,5))
+X_train/=255
+X_test/=255
+
+number_of_classes = 10
+Y_train = np_utils.to_categorical(y_train, number_of_classes)
+Y_test = np_utils.to_categorical(y_test, number_of_classes)
